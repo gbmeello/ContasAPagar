aac247d051f9e51c7e39bbd023f5d959336c7336
gbmello <gabriel_meello@hotmail.com>
2024-09-19 10:07:00 -0300

    Initial commit

---
+++ src/main/java/com/DesafioTotvs/ContasAPagar/ContasAPagarApplication.java
@@ -0,0 +1,13 @@
+package com.DesafioTotvs.ContasAPagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasAPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasAPagarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DesafioTotvs/ContasAPagar/ContasAPagarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesafioTotvs.ContasAPagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasAPagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
